85ca2cb65b44fa831df0b93b306d86c93230fbd4
Deepak Kannan <kannan.deepak@gmail.com>
2013-04-04 18:14:29 +0530

    add some more specs for config

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -6,22 +6,32 @@ describe "Configuration" do
     let(:existing_currency) { Money::Currency.new(:usd) }
     let(:new_currency) { Money::Currency.new(:inr) }
 
-    before(:each) do       
-      Money.default_currency = existing_currency
-    end
-    
-    it "sets default currency" do
-      expect { EasyRailsMoney.default_currency = new_currency }.
-        to change { Money.default_currency }.
-        from(existing_currency).
-        to(new_currency)
+    it "has a default currency" do
+      expect(EasyRailsMoney.default_currency).not_to be_nil
     end
 
-    it "reads default currency" do
-      expect { EasyRailsMoney.default_currency = new_currency }.
-        to change { EasyRailsMoney.default_currency }.
-        from(existing_currency).
-        to(new_currency)
+    it "default currency is equal to money's default_currency" do
+      expect(EasyRailsMoney.default_currency).to eq Money.default_currency
+    end
+    
+    context "can set and read back the config" do
+      before(:each) do
+        Money.default_currency = existing_currency
+      end
+      
+      it "sets default currency" do
+        expect { EasyRailsMoney.default_currency = new_currency }.
+          to change { Money.default_currency }.
+          from(existing_currency).
+          to(new_currency)
+      end
+
+      it "reads default currency" do
+        expect { EasyRailsMoney.default_currency = new_currency }.
+          to change { EasyRailsMoney.default_currency }.
+          from(existing_currency).
+          to(new_currency)
+      end
     end
   end
   
